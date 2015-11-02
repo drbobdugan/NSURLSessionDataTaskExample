3f15a495a1690ba5e7c0ffc3104ba9bd0fd8733d
drbobdugan <bdugan@stonehill.edu>
2015-11-02 12:12:19 -0500

    Initial Commit

---
+++ NSURLSessionDataTaskExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSURLSessionDataTaskExample
+//
+//  Created by Bob Dugan on 11/2/15.
+//  Copyright © 2015 Bob Dugan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
